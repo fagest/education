8bccba6163e668ac040e0914fba841277a945ecd
Макаров Сергей Витальевич <smakarov@estp.sro>
2018-07-17 15:02:56 +0300

    test 3

---
+++ MyApplication/app/src/main/java/com/example/student_39/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.student_39.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.numetriclabz.numandroidcharts.R.styleable.DonutChart
+
+/**
+ * Тестовое задание 1
+ * created 13.07.2018 by Sergey V.Makarov email: fagest@yandex.ru
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_)
+        /*
+        DonutChart donut = (DonutChart) findViewById(R.id.piegraph)
+        ChartData values = new ChartData()
+        values.setSectorValue(4)
+        donut.addSector(values)
+        values = new ChartData()
+        values.setSectorValue(3)
+        donut.addSector(values)
+        values = new ChartData()
+        values.setSectorValue(8)
+        donut.addSector(values)
+        values = new ChartData()
+        values.setSectorValue(5)
+        donut.addSector(values)*/
+    }
+}
