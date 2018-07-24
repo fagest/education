9ab9bee0ea419c3f2fc704fd48f28e1ef3e6965c
Макаров Сергей Витальевич <smakarov@estp.sro>
2018-07-24 18:03:10 +0300

    create project

---
+++ Test10/app/src/main/java/com/example/smakarov/test10/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.smakarov.test10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * тестовое задание 10
+ * created 24.07.2018 by Sergey V.Makarov
+ * email:fagest@yandex.ru
+ *
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
